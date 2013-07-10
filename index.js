6b6884c117ef43b8dba6c3c7066e47486451b75e
Ershov Konstantin <ershov.konst@gmail.com>
2013-07-10 11:01:07 +0400

    configuration for development/production

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ var express = require('express');
 var app = express();
 var requirejs = require('requirejs');
 
-
-app.use(express.static("./public_build"));
-
-requirejs.optimize({
-   appDir : "./public",
-   baseUrl: "lib",
-   dir : "./public_build",
-   modules : [
-      {
-         name : "../modules/module1"
-      },
-      {
-         name : "../modules/module2"
-      }
-   ]
-}, function(){
+function runServer(){
    app.listen(777);
    console.log("port : 777");
-});
+}
 
+app.configure('production', function(){
+   app.use(express.static("./public_build"));
+   requirejs.optimize({
+      appDir : "./public",
+      baseUrl: "lib",
+      dir : "./public_build",
+      modules : [
+         {
+            name : "../modules/module1"
+         },
+         {
+            name : "../modules/module2"
+         }
+      ]
+   }, runServer);
+});
+app.configure('development', function(){
+   app.use(express.static("./public"));
+   runServer();
+});
 
 
 
