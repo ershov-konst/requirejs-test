f4987bb641f3dfc4df8b251f02cd83b90ea40473
Ershov Konstantin <ershov.konst@gmail.com>
2013-07-10 10:23:12 +0400

    примерная структура

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var requirejs = require('requirejs');
 
 
 app.use(express.static("./public"));
 
+requirejs.optimize({
+   /*appDir : "./public",*/
+   baseUrl: "./public/lib",
+   name : "modules/module1",
+   out : "modules/module1-build.js"
+   /*dir : "./public_build",
+   modules : [
+      "modules/module1",
+      "modules/module2"
+   ]*/
+}, function(){
+   app.listen(777);
+   console.log("port : 777");
+});
 
 
-app.listen(777);
+
+
