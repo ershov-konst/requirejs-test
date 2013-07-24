a18c7efa7fb6636eea844e4684f500c46c4ebc96
Ershov Konstantin <ershov.konst@gmail.com>
2013-07-24 10:09:59 +0400

    example of requirejs to load the module and optimization in a single file

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ var express = require('express');
 var app = express();
 var requirejs = require('requirejs');
 
+requirejs.config({
+   baseUrl: "./public/lib/"
+});
+
 app.use(express.static("./public_build"));
+app.get("/", function(){
+   requirejs(["a"], function(a){
+      a.test();
+   })
+});
 /**
  * Предварительная оптимизация перечисленных модулей
  */
-requirejs.optimize({
-   appDir : "./public",
-   mainConfigFile : "./public/main.js",
-   dir : "./public_build",
-   modules : [
-      {
-         name : "../modules/module1"
-      }
-   ]
-}, function(){
-
-   /**
-    * Пример сборки набора модулей
-    */
+requirejs(["a"], function(a){
+   a.test();
    requirejs.optimize({
+      appDir : "./public",
       mainConfigFile : "./public/main.js",
-      include : ["a", "d", "super!b"],
-      out : "./public_build/modules/module3-opt.js"
+      dir : "./public_build",
+      modules : [
+         {
+            name : "../modules/module1"
+         }
+      ]
    }, function(){
-      app.listen(777);
-      console.log("port : 777");
+
+      /**
+       * Пример сборки набора модулей
+       */
+      requirejs.optimize({
+         mainConfigFile : "./public/main.js",
+         include : ["a", "d", "super!b"],
+         out : "./public_build/modules/module3-opt.js"
+      }, function(){
+         app.listen(777);
+         console.log("port : 777");
+      });
    });
 });
+
 
 
 
--- public/lib/a.js
+++ public/lib/a.js
@@ -1,3 +1,8 @@
 define("a", ["b"], function(){
    console.log("a");
+   return {
+      test : function(){
+         console.log("module a - test method");
+      }
+   }
 });
